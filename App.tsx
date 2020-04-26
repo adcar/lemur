d1b7450c3cb96d482b7f6041de64cb3c1c645232
adcar <alexanderdcardosi@gmail.com>
2020-04-26 17:38:27 -0400

    No more header

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./components/Tabs";
+import Login from "./screens/Login";
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tabs />
+      <Stack.Navigator headerMode={"none"}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Main" component={Tabs} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }

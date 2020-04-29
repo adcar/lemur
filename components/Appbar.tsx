5b79652afd3df0e9e636565b42e5ccfb7983002f
adcar <alexanderdcardosi@gmail.com>
2020-04-28 20:15:55 -0400

    Fixed status bar on android

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { Appbar } from "react-native-paper";
 import { DrawerActions } from "@react-navigation/native";
+import Constants from "expo-constants";
+
 export default function MyAppBar({ navigation }: any) {
   return (
-    <Appbar>
+    <Appbar
+      style={{
+        marginTop: Constants.statusBarHeight,
+      }}
+    >
       <Appbar.Action
         icon="menu"
         onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}

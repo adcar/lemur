1031fe5562b04374905a66601ea1583993f85500
Rahavee <rmanian@uvm.edu>
2020-05-05 16:41:49 -0400

    API now gets followed communities

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,31 @@
 function getBase(server: string) {
-  return `https://corsssssss.herokuapp.com/https://${server}/api/v1`;
+    return `https://corsssssss.herokuapp.com/https://${server}/api/v1`;
 }
 
 const headers = {
-  Accept: "application/json",
-  "Content-Type": "application/json",
+    Accept: "application/json",
+    "Content-Type": "application/json",
 };
 
 export async function getPosts(
-  jwt: string,
-  type = "Subscribed",
-  page = 1,
-  sort = "Hot",
-  server = "dev.lemmy.ml"
+    jwt: string,
+    type = "Subscribed",
+    page = 1,
+    sort = "Hot",
+    server = "dev.lemmy.ml"
 ) {
-  const apiBase = getBase(server);
-  const res = await fetch(
-    apiBase + `/post/list?type_=${type}&auth=${jwt}&page=${page}&sort=${sort}`
-  );
-  return (await res.json()).posts;
+    const apiBase = getBase(server);
+    const res = await fetch(
+        apiBase + `/post/list?type_=${type}&auth=${jwt}&page=${page}&sort=${sort}`
+    );
+    return (await res.json()).posts;
 }
 
 export async function getPost(
-  id: number,
-  server = "dev.lemmy.ml",
-  jwt: string,
-  sort: "Hot" | "Top" | "New" | "Old" = "Hot"
+    id: number,
+    server = "dev.lemmy.ml",
+    jwt: string,
+    sort: "Hot" | "Top" | "New" | "Old" = "Hot"
 ) {
   const apiBase = getBase(server);
   const res = await fetch(apiBase + `/post?id=${id}&sort=${sort}&auth=${jwt}`);
@@ -33,48 +33,55 @@ export async function getPost(
 }
 
 export async function login(
-  username: string,
-  password: string,
-  server: string
+    username: string,
+    password: string,
+    server: string
 ): Promise<Response> {
-  const apiBase = getBase(server);
-  console.log("url" + apiBase + "/user/login");
-  return await fetch(apiBase + "/user/login", {
-    method: "POST",
-    headers,
-    body: JSON.stringify({
-      username_or_email: username,
-      password: password,
-    }),
-  });
+    const apiBase = getBase(server);
+    console.log("url" + apiBase + "/user/login");
+    return await fetch(apiBase + "/user/login", {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+            username_or_email: username,
+            password: password,
+        }),
+    });
 }
 
 export async function upvote(id: number, jwt: string, server: string) {
-  const { my_vote, score } = await vote(1, id, jwt, server);
-  return { my_vote, score };
+    const {my_vote, score} = await vote(1, id, jwt, server);
+    return {my_vote, score};
 }
 
 export async function downvote(id: number, jwt: string, server: string) {
-  const { my_vote, score } = await vote(-1, id, jwt, server);
-  return { my_vote, score };
+    const {my_vote, score} = await vote(-1, id, jwt, server);
+    return {my_vote, score};
 }
 
 export async function undovote(id: number, jwt: string, server: string) {
-  const { my_vote, score } = await vote(0, id, jwt, server);
-  return { my_vote, score };
+    const {my_vote, score} = await vote(0, id, jwt, server);
+    return {my_vote, score};
 }
 
 async function vote(vote: number, id: number, jwt: string, server: string) {
-  const apiBase = getBase(server);
-  const res = await fetch(apiBase + "/post/like", {
-    method: "POST",
-    headers,
-    body: JSON.stringify({
-      post_id: id,
-      score: vote,
-      auth: jwt,
-    }),
-  });
+    const apiBase = getBase(server);
+    const res = await fetch(apiBase + "/post/like", {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+            post_id: id,
+            score: vote,
+            auth: jwt,
+        }),
+    });
 
-  return (await res.json()).post;
+    return (await res.json()).post;
+}
+
+//returns a list of communities the user follows
+export async function followedCommunities(jwt: string, server: string) {
+    const apiBase = getBase(server);
+    const res = await fetch(apiBase + `/user/followed_communities?auth=${jwt}`);
+    return (await res.json()).communities;
 }
--- screens/MyCommunities.tsx
+++ screens/MyCommunities.tsx
@@ -1,7 +1,29 @@
-import { Text, View } from "react-native";
-import * as React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { FlatList, View, Text } from "react-native";
+import { followedCommunities } from "../api";
+import { Context } from "../components/Store";
+
+
+
+
 
 export default function MyCommunities() {
+    const [state] = useContext(Context);
+    let [communities, setCommunities]=useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const communities = await followedCommunities(
+                state.jwt,
+                state.server
+            );
+
+            setCommunities(communities);
+        })();
+    }, [state]);
+
+    console.log(communities);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>My communities!</Text>

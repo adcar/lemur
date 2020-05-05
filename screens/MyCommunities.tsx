d88dc144c20f6ab6ce5ca44aabd78411b461d54f
Rahavee <rmanian@uvm.edu>
2020-05-05 18:44:29 -0400

    Mycommunities displays communities and is searchable

--- screens/MyCommunities.tsx
+++ screens/MyCommunities.tsx
@@ -1,15 +1,16 @@
-import React, { useContext, useEffect, useState } from "react";
-import { FlatList, View, Text } from "react-native";
-import { followedCommunities } from "../api";
-import { Context } from "../components/Store";
+import React, {useContext, useEffect, useState} from "react";
+import {View, Text} from "react-native";
+import {Appbar, List, Searchbar} from "react-native-paper";
+import {followedCommunities} from "../api";
+import {Context} from "../components/Store";
+import Constants from "expo-constants";
 
 
-
-
-
-export default function MyCommunities() {
+export default function MyCommunities({navigation}: any) {
     const [state] = useContext(Context);
-    let [communities, setCommunities]=useState([]);
+    let [communities, setCommunities] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         (async () => {
@@ -19,14 +20,50 @@ export default function MyCommunities() {
             );
 
             setCommunities(communities);
+            setResults(communities);
         })();
     }, [state]);
 
-    console.log(communities);
 
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>My communities!</Text>
-    </View>
-  );
+    function search(text: string) {
+        let newlist = [];
+        const regex = new RegExp(text, "i");
+        communities.map((community, index) => {
+            if (regex.test(community.community_name)) {
+                console.log('in');
+                newlist.push(community);
+            }
+        });
+        setResults(newlist);
+    }
+
+
+    return (
+        <View style={{flex: 1}}>
+            <Appbar
+                style={{
+                    marginTop: Constants.statusBarHeight,
+                }}
+            >
+                <Searchbar
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChangeText={(text) => {
+                        setSearchTerm(text);
+                        search(text);
+
+                    }}/>
+
+            </Appbar>
+            <List.Section>
+                {results.map((community, index) => (
+                    <List.Item
+                        style={{backgroundColor: '#272727', marginTop: 7}}
+                        title={community.community_name}
+                        key={index}
+                    />
+                ))}
+            </List.Section>
+        </View>
+    );
 }
